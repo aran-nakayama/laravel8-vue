c08ab1aaf2cd298d8803c80a85e8eb9bc0c0dac6
pieru <aran0204.ambro@icloud.com>
2021-03-05 10:46:43 +0900

    create function for checking whether liked

--- laravel/app/Models/Article.php
+++ laravel/app/Models/Article.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use phpDocumentor\Reflection\Types\Boolean;
 
 class Article extends Model
 {
@@ -19,5 +21,17 @@ class Article extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\User','likes')->withTimestamps();
+    }
+
+    public function isLikedBy(?User $user): Bool
+    {
+        return $user
+        ? (bool)$this->likes->where('id', $user->id)->count()
+        :false;
     }
 }
