3766b296c5e55944196abc92dc6942e7724e1d3c
pieru <aran0204.ambro@icloud.com>
2021-03-24 10:21:36 +0900

    correction by review

--- laravel/resources/views/nav.blade.php
+++ laravel/resources/views/nav.blade.php
@@ -21,18 +21,25 @@
 
             <!-- Dropdown -->
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                <a class="nav-link dropdown-toggle"
+                    id="navbarDropdownMenuLink"
+                    data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="false">
                     <i class="fas fa-user-circle"></i>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-                    <button class="dropdown-item" type="button"
+                    <button
+                        class="dropdown-item"
+                        type="button"
                         onclick="location.href='{{ route('users.show',['name' => Auth::user()->name]) }}'">
-                    {{ __('messages.mypage') }}
+                        {{ __('messages.mypage') }}
                     </button>
                     <div class="dropdown-divider"></div>
-                    <button form="logout-button" class="dropdown-item" type="submit">
-                    {{ __('messages.logout') }}
+                    <button
+                        form="logout-button"
+                        class="dropdown-item"
+                        type="submit">
+                        {{ __('messages.logout') }}
                     </button>
                 </div>
             </li>
